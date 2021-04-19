6ae023f28055d741911b6e186092021812f76413
rosenmai <20245897+maikroservice@users.noreply.github.com>
2021-04-19 21:47:06 +0200

    added card styling + theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple-light': "hsl(236, 72%, 79%)",
+        'purple-dark': "hsl(237, 63%, 64%)",
+        'very-light-gray': "hsl(240, 78%, 98%)",
+        'light-grayish-blue' : "hsl(234, 14%, 74%)",
+        'grayish-blue': "hsl(233, 13%, 49%)",
+        'dark-grayish-blue': "hsl(232, 13%, 33%)"
+      },
+      backgroundImage: theme => ({
+      'top-right': "url('../assets/images/bg-top.svg')",
+      'bottom-left': "url('../assets/images/bg-bottom.svg')"
+    })},
   },
   variants: {
     extend: {},
